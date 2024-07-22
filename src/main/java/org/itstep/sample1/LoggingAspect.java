9e22cbd060a129f2504944b8668315824a7a26ab
gk <lelizaveta776@gmail.com>
2024-07-22 21:30:31 +0300

    lala

---
+++ src/main/java/org/itstep/sample1/LoggingAspect.java
@@ -0,0 +1,47 @@
+package org.itstep.sample1;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.boot.logging.java.SimpleFormatter;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+@Aspect
+public class LoggingAspect {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+    {
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        handler.setFormatter(new SimpleFormatter());
+        //logger.addHandler(handler);
+    }
+
+    @Pointcut("within(org.itstep.FullName)")
+    public void stringProcessingMethods(){}
+
+    @After("stringProcessingMethods()")
+    public void logMethodCall(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        logger.log(Level.INFO, "название метода: " + methodName);
+    }
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        logger.log(Level.INFO, joinPoint.getSignature() + " выполнен за " + executionTime + " мс");
+        return proceed;
+    }
+
+    @AfterReturning(pointcut = "execution(public String org.itstep.FullName.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        logger.log(Level.INFO, "возвращенное значение: " + result.toString());
+    }
+
+}
